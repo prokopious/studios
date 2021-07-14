8b515cf22f311ce60dd33c30af0776b05ef7333d
tmhuyett <toddhuyett@gmail.com>
2021-07-14 10:07:51 -0400

    'd'

--- pages/contact.js
+++ pages/contact.js
@@ -1,73 +1,10 @@
-import Layout from "../components/Layout"
+import ContactForm from '../components/ContactForm'
 
-const About = () => (
-  <Layout title="About">
-    <form
-      form-name="contact"
-      data-netlify-recaptcha="true"
-      method="POST"
-      data-netlify="true"
-    >
-      <p>
-        <label>
-          Your Name: <input type="text" name="name" />
-        </label>
-      </p>
-      <p>
-        <label>
-          Your Email: <input type="email" name="email" />
-        </label>
-      </p>
-      <p>
-        <label>
-          Your Role:{" "}
-          <select name="role[]" multiple>
-            <option value="leader">Leader</option>
-            <option value="follower">Follower</option>
-          </select>
-        </label>
-      </p>
-      <p>
-        <label>
-          Message: <textarea name="message"></textarea>
-        </label>
-      </p>
-      <p>
-        <button type="submit">Send</button>
-      </p>
-    </form>
-    <style jsx>{`
-      .company-info {
-        margin: 0 0 50px 0;
-        padding: 0;
-        list-style: none;
-        font-size: 18px;
-      }
-      .company {
-        font-weight: 700;
-        font-size: 24px;
-      }
-      .about-text {
-        color: #777;
-        text-align: left;
-        max-width: 800px;
-        margin: 0 auto 100px;
-        padding: 20px;
-      }
-      img {
-        float: right;
-        margin: 10px 0 20px 20px;
-        width: 100x;
-      }
-      @media (max-width: 600px) {
-        .about-text img {
-          float: none;
-          display: block;
-          margin: 0 auto 30px;
-        }
-      }
-    `}</style>
-  </Layout>
-)
 
-export default About
+export default function Contact() {
+  return (
+    <div>
+       <ContactForm />
+    </div>
+  )
+}
